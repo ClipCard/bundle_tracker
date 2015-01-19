fbe9aa0393cc10691026e8cd1ea76a5aae281029
eyelidlessness <gnosis@gmail.com>
2015-01-18 19:34:50 -0800

    Docstrings/tags for markdown ns

--- src/bundle_tracker/markdown.clj
+++ src/bundle_tracker/markdown.clj
@@ -1,28 +1,48 @@
 (ns bundle-tracker.markdown
   (:require [clojure.string :as string]))
 
-(defn join-extensions [[description extensions]]
+(defn join-extensions
+  ^{:doc "Returns a key-value pair where `extensions` has been joined and
+         formatted as Markdown code."
+    :tag clojure.lang.APersistentVector}
+  [[description extensions]]
   (let [with-backticks (map #(str "`" % "`") extensions)]
     [description (string/join ", " with-backticks)]))
 
-(defn longest-string [items]
+(defn longest-string
+  ^{:doc "Determines the longest string in a sequence."
+    :tag Number}
+  [items]
   (reduce #(max %1 (count %2)) 0 items))
 
-(defn fill-string [length c]
+(defn fill-string
+  ^{:doc "Returns a string filled with character `c` up to `length`"
+    :tag java.lang.String}
+  [length c]
   (apply str (repeat length c)))
 
-(defn pad-string [length s]
+(defn pad-string
+  ^{:doc "Returns a string padded with whitespace to `length`
+         (padding is added on the right)."
+    :tag java.lang.String}
+  [length s]
   (let [pad-length (- length (count s))
         padding (fill-string pad-length " ")]
     (str s padding)))
 
-(defn table [headers body]
+(defn table
+  ^{:doc "Returns a Markdown (GFM) formatted table."
+    :tag java.lang.String}
+  [headers body]
   (let [separators (map #(fill-string (count %) "-") headers)
         sections (concat [headers] [separators] body)
         rows (map #(string/join " | " %) sections)]
     (string/join "\n" rows)))
 
 (defn bundle-types->table
+  ^{:doc "Returns a Markdown (GFM) formatted table of `types` derived
+         from a LaunchServices dump."
+    :tag java.lang.String}
   [types]
   (let [with-joined-extensions (map join-extensions types)
         descriptions (map first with-joined-extensions)

57b7cbd21f1cf7f08b489b39e67abe53e08a74d0
eyelidlessness <gnosis@gmail.com>
2015-01-18 17:02:54 -0800

    Include EDN output

--- src/bundle_tracker/core.clj
+++ src/bundle_tracker/core.clj
@@ -1,17 +1,28 @@
 (ns bundle-tracker.core
-  (:require [bundle-tracker.bundle :as bundle]
+  (:require [clojure.pprint :refer [pprint]]
+            [bundle-tracker.bundle :as bundle]
             [bundle-tracker.launch-services :as ls]
             [bundle-tracker.markdown :as markdown])
   (:gen-class))
+
+(defn pretty-print-edn [x]
+  (with-out-str (pprint x)))
+
+(defn save [types]
+  (println "Saving known_types.edn")
+  (spit "known_types.edn" (pretty-print-edn types))
+
+  (println "Saving KNOWN_TYPES.md")
+  (let [table (markdown/bundle-types->table types)
+        md (str "# Known bundle types #\n\n" table "\n")]
+    (spit "KNOWN_TYPES.md" md)))
 
 (defn -main [& args]
   (let [opts (into #{} args)
         save? (opts ":save")
         dump (ls/ls-dump)
-        types (bundle/ls-dump->bundle-types dump)
-        table (markdown/bundle-types->table types)
-        result (str "# Known bundle types #\n\n" table "\n")]
+        types (bundle/ls-dump->bundle-types dump)]
     (if save?
-        (spit "KNOWN_TYPES.md" result)
-        (println result))
+        (save types)
+        (pprint types))
     (System/exit 0)))

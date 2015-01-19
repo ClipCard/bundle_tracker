0923f785cc48ac327a932a117062820d2ac0cd54
eyelidlessness <gnosis@gmail.com>
2015-01-18 19:25:14 -0800

    Docstrings, tags, arglists for bundle ns

--- src/bundle_tracker/bundle.clj
+++ src/bundle_tracker/bundle.clj
@@ -2,56 +2,100 @@
   (:require [bundle-tracker.launch-services :as ls]
             [bundle-tracker.overrides :as overrides]))
 
-(def ^:dynamic *known-types*
+(def ^{:dynamic true
+       :doc "Refers current state of known types. This state will be used
+            to produce additive changes as users run the project."}
+  *known-types*
   (read-string (slurp "known_types.edn")))
 
-(def ^:dynamic *bundle-utis*
+(def ^{:dynamic true
+       :doc "References bundle and package UTI types. Can be bound to
+            conforming types to identify bundle extensions."}
+  *bundle-utis*
   #{"com.apple.bundle"
     "com.apple.package"})
 
-(defn conforms? [item]
+(defn conforms?
+  ^{:doc "Determines if an item conforms to *bundle-utils*."
+    :tag Boolean}
+  [item]
   (some *bundle-utis* (item "conforms to")))
 
-(defn with-conforming-uti [acc item]
+(defn with-conforming-uti
+  ^{:doc "Includes an item's `uti` in `acc` if the item conforms to *bundle-utis*."
+    :tag clojure.lang.APersistentSet}
+  [acc item]
   (if (conforms? item)
       (conj acc (item "uti"))
       acc))
 
-(defn conforming-utis [items]
+(defn conforming-utis
+  ^{:doc "Returns all UTIs from `items` conforming to `*bundle-utis*`"
+    :tag clojure.lang.APersistentSet}
+  [items]
   (reduce with-conforming-uti #{} items))
 
-(defn item-description [item]
+(defn item-description
+  ^{:doc "Returns an item's description, falling back to its UTI."
+    :tag java.lang.String}
+  [item]
   (or (item "description")
       (item "uti")))
 
-(defn ext? [x]
+(defn ext?
+  ^{:doc "Determines if `x` looks like an extension string."
+    :tag Boolean}
+  [x]
   (and (string? x) (re-matches #"^\.\S+$" x)))
 
-(defn item-extensions [item]
+(defn item-extensions
+  ^{:doc "Returns a set of `bindings` and `tags` for an item that look like extensions."
+    :tag clojure.lang.APersistentSet}
+  [item]
   (let [{bindings "bindings" tags "tags"} item]
     (into #{} (filter ext? (concat bindings tags)))))
 
-(defn merge-set [acc k v]
+(defn merge-set
+  ^{:doc "Associates a set for `k` where the value is either the combined sets of the
+         original value and `v`, or `v` where there was no original value."
+    :tag clojure.lang.APersistentMap}
+  [acc k v]
   (let [current (acc k)
         merged (if current (into #{} (concat current v)) v)]
     (assoc acc k merged)))
 
-(defn merge-sets [a b]
+(defn merge-sets
+  ^{:doc "Returns a map of sets where `a`'s values are conjoined with `b`, and
+         `b`'s values are provided for keys not present in `a`."
+    :tag clojure.lang.APersistentMap}
+  [a b]
   (reduce #(apply merge-set %1 %2) a b))
 
-(defn with-extensions [acc item]
+(defn with-extensions
+  ^{:doc "Returns a map of descriptions from `item` to a set of values that look
+         like extension strings from `item`'s `bindings` and `tags`, adding to
+         any existing extensions in `acc`."
+    :tag clojure.lang.APersistentMap}
+  [acc item]
   (let [k (item-description item)
         extensions (item-extensions item)]
     (if (empty? extensions)
         acc
         (merge-set acc k extensions))))
 
-(defn with-conforming-type [acc item]
+(defn with-conforming-type
+  ^{:doc "Associates extensions from `item` if it conforms to `*bundle-utis*`."
+    :tag clojure.lang.APersistentMap}
+  [acc item]
   (if (conforms? item)
       (with-extensions acc item)
       acc))
 
-(defn override-descriptions [acc [description extensions]]
+(defn override-descriptions
+  ^{:doc "Where override descriptions are provided, replaces existing mappings
+         of description->extensions with overrides."
+    :tag clojure.lang.APersistentMap}
+  [acc [description extensions]]
   (if-let [override (overrides/*description* description)]
     (-> acc
         (assoc override extensions)
@@ -59,6 +103,10 @@
     acc))
 
 (defn ls-dump->bundle-types
+  ^{:arglists [dump]
+    :doc "Produces a sequence of bundle types `[description extensions]` for
+         a LaunchServices dump."
+    :tag clojure.lang.ASeq}
   ([dump]
     (let [items (ls/read-ls-types dump)]
       (ls-dump->bundle-types *bundle-utis* items)))
